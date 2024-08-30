981d30bd0e4cf8fc314a6360de932c50ea56e6ca
gyenoch <www.gyenoch@gmail.com>
2024-08-30 03:12:03 +0000

    eks cluster code updated

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -2,8 +2,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = var.eksnode-group-name
   node_role_arn   = aws_iam_role.NodeGroupRole.arn
-  subnet_ids      = [data.aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
-
+  subnet_ids      = [aws_subnet.public-subnet1.id, aws_subnet.public-subnet2.id]
 
   scaling_config {
     desired_size = 2
