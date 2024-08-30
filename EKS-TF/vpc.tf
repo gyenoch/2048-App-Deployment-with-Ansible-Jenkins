39fadcd47661df427a8963e107a77a84cb732acf
gyenoch <www.gyenoch@gmail.com>
2024-08-30 03:12:35 +0000

    vpc terraform code updated

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -67,38 +67,30 @@ resource "aws_route_table_association" "rt-association2" {
   subnet_id      = aws_subnet.public-subnet2.id
 }
 
+# resource "aws_subnet" "public-subnet2" {
+#   vpc_id                  = data.aws_vpc.vpc.id
+#   cidr_block              = "10.0.2.0/24"
+#   availability_zone       = "us-east-1b"
+#   map_public_ip_on_launch = true
 
-data "aws_security_group" "sg-default" {
-  filter {
-    name   = "tag:Name"
-    values = [var.security-group-name]
-  }
-}
+#   tags = {
+#     Name = var.subnet-name2
+#   }
+# }
 
-resource "aws_subnet" "public-subnet2" {
-  vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+# resource "aws_route_table" "rt2" {
+#   vpc_id = data.aws_vpc.vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = data.aws_internet_gateway.igw.id
+#   }
 
-  tags = {
-    Name = var.subnet-name2
-  }
-}
+#   tags = {
+#     Name = var.rt-name2
+#   }
+# }
 
-resource "aws_route_table" "rt2" {
-  vpc_id = data.aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = var.rt-name2
-  }
-}
-
-resource "aws_route_table_association" "rt-association2" {
-  route_table_id = aws_route_table.rt2.id
-  subnet_id      = aws_subnet.public-subnet2.id
-}
+# resource "aws_route_table_association" "rt-association2" {
+#   route_table_id = aws_route_table.rt2.id
+#   subnet_id      = aws_subnet.public-subnet2.id
+# }
