3f9bf5c49af028210d64f3a357dbe21410c0fc9a
gyenoch <www.gyenoch@gmail.com>
2024-08-30 02:55:49 +0000

    vpc.tf updated

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -1,23 +1,72 @@
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc-name]
+# Create a VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = var.vpc-name
   }
 }
 
-data "aws_internet_gateway" "igw" {
-  filter {
-    name   = "tag:Name"
-    values = [var.igw-name]
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = var.igw-name
   }
 }
 
-data "aws_subnet" "subnet" {
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet-name]
+# Create a Subnet (Public Subnet 1)
+resource "aws_subnet" "public-subnet1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet-name
   }
 }
+
+# Create a Subnet (Public Subnet 2)
+resource "aws_subnet" "public-subnet2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet-name2
+  }
+}
+
+# Create a Security Group
+data "aws_security_group" "sg-default" {
+  filter {
+    name   = "tag:Name"
+    values = [var.security-group-name]
+  }
+}
+
+# Create a Route Table for Subnet 2
+resource "aws_route_table" "rt2" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = var.rt-name2
+  }
+}
+
+# Associate Route Table with Subnet 2
+resource "aws_route_table_association" "rt-association2" {
+  route_table_id = aws_route_table.rt2.id
+  subnet_id      = aws_subnet.public-subnet2.id
+}
+
 
 data "aws_security_group" "sg-default" {
   filter {
