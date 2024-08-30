da2b779bc853a2c97c2aef0e008a6ec8d124d6d4
gyenoch <www.gyenoch@gmail.com>
2024-08-30 03:21:00 +0000

    security group added

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -40,11 +40,43 @@ resource "aws_subnet" "public-subnet2" {
   }
 }
 
-# Create a Security Group
-data "aws_security_group" "sg-default" {
-  filter {
-    name   = "tag:Name"
-    values = [var.security-group-name]
+# # Create a Security Group
+# data "aws_security_group" "sg-default" {
+#   filter {
+#     name   = "tag:Name"
+#     values = [var.security-group-name]
+#   }
+# }
+
+resource "aws_security_group" "sg-default" {
+  name        = "Jenkins-Security-Group"
+  description = "Open 22,443,465,80,8080,9000,9100,9090,3000"
+  vpc_id       = aws_vpc.vpc.id
+
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443,465,8080,9000,9100,9090,3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jenkins-sg"
   }
 }
 
