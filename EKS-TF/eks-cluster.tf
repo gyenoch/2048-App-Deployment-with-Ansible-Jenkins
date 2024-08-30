9931cbb59eb5fbac5f7ddf30093e4bee9cd01440
gyenoch <www.gyenoch@gmail.com>
2024-08-30 03:23:19 +0000

    eks-cluster.tf updated

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "eks-cluster" {
   role_arn = aws_iam_role.EKSClusterRole.arn
   vpc_config {
     subnet_ids         = [aws_subnet.public-subnet1.id, aws_subnet.public-subnet2.id]
-    security_group_ids = [data.aws_security_group.sg-default.id]
+    security_group_ids = [aws_security_group.sg-default.id]
   }
 
   version = 1.28
