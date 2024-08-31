1e2cdb5031673e6a835c514241172877f2ae0734
gyenoch <www.gyenoch@gmail.com>
2024-08-31 11:31:20 +0000

    variables.tf added

---
+++ EC2-TF/variables.tf
@@ -0,0 +1,29 @@
+variable "sg_name" {
+  type = string
+  description = "NAME OF SG"
+}
+
+variable "web_instance_type" {
+  type = string
+  description = "INSTANCE TYPE FOR WEB 1"
+}
+
+variable "key_name" {
+  type = string
+  description = "KEY NAME FOR INSTANCE"
+}
+
+variable "web1_name" {
+  type = string
+  description = "NAME FOR WEB 1"
+}
+
+variable "web2_instance_type" {
+  type = string
+  description = "INSTANCE TYPE FOR WEB 2"
+}
+
+variable "web2_name" {
+  type = string
+  description = "NAME FOR WEB 2"
+}
